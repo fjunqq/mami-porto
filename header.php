6d9c4a4f29bcbc62a41be62c2c7b1be52146b781
fangjun <fjun99@gmail.com>
2014-11-17 00:28:04 +0800

    logo

--- header.php
+++ header.php
@@ -23,11 +23,12 @@
 		<header id="masthead" class="site-header" role="banner">
 			<a class="home-link" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
 				<!--<h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>-->
-				<img src="<?php echo get_template_directory_uri(); ?>/mamifair.png" class="site-logo">
-
+				<img src="<?php echo get_template_directory_uri(); ?>/mamifair.png" class="site-logo" alt="<?php bloginfo('description'); ?>">
+<!--
                 <?php if(get_bloginfo('description') != '') : ?>
 				<h2 class="site-description"><?php bloginfo('description'); ?></h2>
 				<?php endif; ?>
+-->
 			</a>
 		</header><!-- #masthead -->
 				
