2d2bed9907a3509efd3a09d3a90dc1fa40c64f8a
fangjun <fjun99@gmail.com>
2014-11-16 20:33:13 +0800

    first

---
+++ footer.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ */
+ 
+?>
+		</div><!-- #main -->
+	</div><!-- #page -->
+	
+	<footer id="gk-footer" role="contentinfo">
+		<?php if (is_active_sidebar('bottom')) : ?>
+		<div id="gk-bottom" role="complementary">
+			<div class="widget-area">
+				<?php dynamic_sidebar('bottom'); ?>
+			</div>
+		</div>
+		<?php endif; ?>
+		
+		<div id="gk-social">
+			<?php wp_nav_menu(array('theme_location' => 'footer', 'menu_class' => 'social-menu')); ?>
+		</div>
+		
+		<div id="gk-copyrights">
+			<?php do_action('portfolio_credits'); ?>
+			<p class="copyright">Made With Love. <a href="http://mamifair.com/"> MamiFair </a></p>			
+		</div><!-- .site-info -->
+	</footer><!-- end of #gk-footer -->
+	
+	<?php wp_footer(); ?>
+</body>
+</html>
