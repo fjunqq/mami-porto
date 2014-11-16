5f2da7f76455a75cc1c709445741b55d716ec345
fangjun <fjun99@gmail.com>
2014-11-17 00:31:12 +0800

    logo

--- header.php
+++ header.php
@@ -21,9 +21,9 @@
 	<div id="ie-toolbar"><div><?php _e('You\'re using an unsupported version of Internet Explorer. Please <a href="http://windows.microsoft.com/en-us/internet-explorer/products/ie/home">upgrade your browser</a> for the best user experience on our site. Thank you.', 'portfolio') ?></div></div>
 	<![endif]-->
 		<header id="masthead" class="site-header" role="banner">
-			<a class="home-link" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+			<a class="home-link" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php bloginfo('description'); ?>" rel="home">
 				<!--<h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>-->
-				<img src="<?php echo get_template_directory_uri(); ?>/mamifair.png" class="site-logo" alt="<?php bloginfo('description'); ?>">
+				<img src="<?php echo get_template_directory_uri(); ?>/mamifair.png" class="site-logo">
 <!--
                 <?php if(get_bloginfo('description') != '') : ?>
 				<h2 class="site-description"><?php bloginfo('description'); ?></h2>
